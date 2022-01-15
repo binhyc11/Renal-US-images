bf66f3b5451d1ee98f5822f33f6e1dd5c24cd8d8
binhyc11 <90891898+binhyc11@users.noreply.github.com>
2022-01-16 01:28:57 +0900

    Update patient_list_w_complete_data.py

--- patient_list_w_complete_data.py
+++ patient_list_w_complete_data.py
@@ -16,21 +16,22 @@ for i in range (len(rows)):
 def unique ():
     directory = 'D:/renalUS/data/'
     IDs = os.listdir(directory)
-    unique_kidney =[]
+    uni =[]
+
     for ID in IDs:
         files = os.listdir(directory + ID)
         only1 = False
-        mask_basename = ''
+        names = ''
         for file in files:
             f_name, file_ext = os.path.splitext (file)
             base = os.path.basename(f_name)
             if file_ext == '.nrrd':
-                name  = mask_basename + base
-        if 'l' and 'r' not in name:
+                names = names + base
+        if ('l' and 'r' not in names) == True:
             only1 = True
         if only1 == True:
-            unique_kidney.append(ID)
-    return unique_kidney        
+            uni.append(ID)
+    return uni        
         
 with open ('C:/Users/binhy/Desktop/patients.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
