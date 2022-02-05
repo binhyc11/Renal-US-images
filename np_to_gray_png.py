586c4d11b69a2347bb0536076c311e80863a2687
binhyc11 <90891898+binhyc11@users.noreply.github.com>
2022-02-05 20:21:54 +0900

    Create np_to_gray_png.py

---
+++ np_to_gray_png.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+files = os.listdir('D:/np_ext/')
+for i in files:
+    img  = np.load ('D:/np_ext/25.npy')
+    plt.imshow (img, cmap="gray")
+    plt.axis('off')
+    plt.savefig('D:/ext/%s.png' %i, bbox_inches='tight',pad_inches = 0)
