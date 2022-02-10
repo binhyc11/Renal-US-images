f3c3bb6ef6c50e66e74a989943ba305959ff7db3
binhyc11 <90891898+binhyc11@users.noreply.github.com>
2022-02-10 19:16:44 +0900

    Update Train_test_split.py

--- Train_test_split.py
+++ Train_test_split.py
@@ -1,29 +1,70 @@
-import os, shutil
-import numpy as np
+import os, shutil, csv
 from sklearn.model_selection import train_test_split
 
-u55 = os.listdir('D:/renalUS/exp_U55')
-b65 = os.listdir('D:/renalUS/exp_B65')
+ID_55_65 = []
+list_55_65 = os.listdir('D:/data/ext_in_range_55_65/')
+for i in list_55_65:
+    ID = i[:8]
+    if ID not in ID_55_65:
+        ID_55_65.append (ID)
 
-data =[]
-label = []
+with open ('C:/Users/binhy/Desktop/patients_list.csv') as file:
+    csvreader = csv.reader (file)
+    header = []
+    header = next(csvreader)
+    rows = []
+    for row in csvreader:
+        rows.append (row)
 
-for i in u55:
-    data.append('D:/renalUS/exp_U55/' + i)
-    label.append(5)
+path_0, path_1 = [], []
+        
+for i in rows:
+    for j in list_55_65:
+        path_j = 'D:/data/ext_in_range_55_65/' + j
+        ID = j[:8]
+        if ID == i[0]:
+            if i[1] == '0':
+                path_0.append(path_j)
+            else:
+                path_1.append(path_j)
 
-for i in b65:
-    data.append('D:/renalUS/exp_B65/' + i)
-    label.append(6)
+test_5 = os.listdir('D:/data/test_exp/5/')
+for i in test_5:
+    path = 'D:/data/test_exp/5/' + i
+    path_0.append (path)
 
-X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2)
+train_5 = os.listdir('D:/data/train_exp/5/')
+for i in train_5:
+    path = 'D:/data/train_exp/5/' + i
+    path_0.append (path)
 
-for i in range (len(X_train)):
-    basename = os.path.splitext(os.path.basename (X_train[i]))[0]
-    basename = os.path.splitext (basename)[0]
-    shutil.copyfile(X_train[i], 'D:/train/' + str(y_train[i]) + '_' + basename + '.png')
+test_6 = os.listdir('D:/data/test_exp/6/')
+for i in test_6:
+    path = 'D:/data/test_exp/6/' + i
+    path_1.append (path)
+
+train_6 = os.listdir('D:/data/train_exp/6/')
+for i in train_6:
+    path = 'D:/data/train_exp/6/' + i
+    path_1.append (path)
     
-for i in range (len(X_test)):
-    basename = os.path.splitext(os.path.basename (X_test[i]))[0]
-    basename = os.path.splitext (basename)[0]
-    shutil.copyfile(X_test[i], 'D:/test/' + str(y_test[i]) + '_' + basename + '.png')
+
+
+train_0, test_0 = train_test_split(path_0, test_size=0.2)
+train_1, test_1 = train_test_split(path_1, test_size=0.2)
+
+for i in train_0:
+    name = os.path.basename (i)
+    shutil.copyfile(i, 'D:/train/0/' + name)
+
+for i in test_0:
+    name = os.path.basename (i)
+    shutil.copyfile(i, 'D:/test/0/' + name)
+
+for i in train_1:
+    name = os.path.basename (i)
+    shutil.copyfile(i, 'D:/train/1/' + name)
+
+for i in test_1:
+    name = os.path.basename (i)
+    shutil.copyfile(i, 'D:/test/1/' + name)
