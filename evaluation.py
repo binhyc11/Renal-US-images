2f98c7998f46659311b538ed0d0b8a878e1b2c86
binhyc11 <90891898+binhyc11@users.noreply.github.com>
2022-02-10 09:58:45 +0900

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,14 +1,9 @@
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
-import os
+import os, csv
 
-def prediction (path):    
-    model = load_model("D:/2_layers_best.h5")
-    model.compile (loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    
+def prediction (path, model):    
     img = image.load_img(path, target_size=(195, 334))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -16,17 +11,29 @@ def prediction (path):
     images = np.vstack([x])
     classes = model.predict(images)
     return (classes[0][0])
-    
 
-list_ext = os.listdir('D:/ext_heather/')
+    
+model = load_model("D:/Conv_32x9+64x7_best.h5")
+model.compile (loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+list_ext = os.listdir('D:/data/ext_VN_complete/')
 
 result = []
 
 for i in list_ext:
-    path = 'D:/ext_heather/' + str(i)
-    pred = prediction(path)
+    path = 'D:/data/ext_VN_complete/' + str(i)
+    pred = prediction(path, model)
     print (pred)
     temp = []
     temp.append (i)
     temp.append (pred)
     result.append(temp)
+
+
+header = ['ID', 'Pred']
+with open ('C:/Users/binhy/Desktop/patients.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(result)
